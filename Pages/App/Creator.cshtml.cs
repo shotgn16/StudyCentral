187b3a2d7b4f6b7976697ab5076da07f79f86641
Jack Barnard <jack.tm68@googlemail.com>
2023-07-17 22:56:15 +0100

    Re-styled login and register page

---
+++ Pages/App/Creator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudyCentralV2.Pages.App
+{
+    public class CreatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
